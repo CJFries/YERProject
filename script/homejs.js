49a6edd313ad446ba66cbfd1d48d0d3cad00d0dd
JWSchippers <schippers.jw@gmail.com>
2019-10-21 15:27:35 +0200

    frontend functionaliteit

--- script/homejs.js
+++ script/homejs.js
@@ -1,3 +1,53 @@
-function test() {
-    alert("Warme Bal");
+function ophalen() {
+    alert("opgehaald");
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+
+    }
+    xhr.open("GET", "http://localhost:8082/allsquashclubmembers", true);
+    xhr.send();
+    xhr.onload = function() {
+        if(this.readyState == 4) {
+            var haalGegevensJSON = this.responseText;
+            var haalGegevens = JSON.parse(haalGegevensJSON);
+            console.log(haalGegevens)
+            document.getElementById("id").innerHTML = haalGegevens.id;
+            document.getElementById("vnhaal").innerHTML = haalGegevens.firstName;
+            document.getElementById("anhaal").innerHTML = haalGegevens.lastName;
+            
+        }
+    }
+}
+
+function versturen() {
+    alert("verstuurd")
+    var voornaam = document.getElementById("vn").value;
+    var achternaam = document.getElementById("an").value;
+    var rating = document.getElementById("rating").value;
+    var geslacht = document.getElementById("geslacht").value;
+    var geboortedatum = document.getElementById("gebdat").value;
+
+    var mijnGegevensObject = {}
+    mijnGegevensObject.firstName = voornaam;
+    mijnGegevensObject.lastName = achternaam;
+    mijnGegevensObject.rating = rating;
+    mijnGegevensObject.gender = geslacht;
+    mijnGegevensObject.dateOfBirth = geboortedatum;
+
+    var mijnGegevens = JSON.stringify(mijnGegevensObject)
+    console.log(mijnGegevens);
+    postData(mijnGegevens);    
+}
+
+function postData(data) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 202) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", "http://localhost:8082/allsquashclubmembers", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(data);
+    console.log(data);
 }
